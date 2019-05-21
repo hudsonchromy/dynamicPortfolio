340155c9b74e364bbbfc44a71d677747ce308804
hudsonchromy <hjc0018@auburn.edu>
2019-05-21 14:28:01 -0500

    fixing path

--- portfolio/index.php
+++ portfolio/index.php
@@ -3,13 +3,12 @@
         <title>
         Hudson Chromy
         </title>
-        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" type="text/css" href="../style.css">
         <link rel="stylesheet" href="https://cdn.auburn.edu/assets/css/bootstrap.min.css"/>
         <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Kadwa:700&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Inconsolata:700&display=swap" rel="stylesheet">
-        <link href="http://thawing-journey-11836.herokuapp.com/style.css">
     </head>
 <body>
     <?php include('../sidebar.php')?>
