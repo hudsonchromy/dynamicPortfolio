15c88e8500216d2eb145c60b5541930611b00077
hudsonchromy <hjc0018@auburn.edu>
2019-05-21 14:13:03 -0500

    updated style path

--- portfolio/index.php
+++ portfolio/index.php
@@ -9,7 +9,7 @@
         <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Kadwa:700&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Inconsolata:700&display=swap" rel="stylesheet">
-        <link href="style.css">
+        <link href="../style.css">
     </head>
 <body>
     <?php include('../sidebar.php')?>
