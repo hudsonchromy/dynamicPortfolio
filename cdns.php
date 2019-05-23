9ee24656a78b3954eff07c0dd8c82e8bb17c998c
hudsonchromy <hjc0018@auburn.edu>
2019-05-23 09:33:53 -0500

    added cdns

--- cdns.php
+++ cdns.php
@@ -1,5 +1,7 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-139847415-2"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/trianglify/1.1.0/trianglify.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/trianglify/0.4.0/trianglify.min.js"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
