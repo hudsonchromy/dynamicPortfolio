b51591244add4bf393b1de8012a639345324c741
hudsonchromy <hjc0018@auburn.edu>
2019-05-23 10:53:48 -0500

    bettering style

--- index.php
+++ index.php
@@ -1,27 +1,6 @@
 <html>
 <style>
-html {
-    height: 100%;
-    width: 100%;
-    margin: 0;
-    overflow: hidden;
-}
-#myContainer {
-  width: 100%;
-  height: 100%;
-  position: relative;
-  background: yellow;
-}
-#myAnimation {
-    z-index: 10;
-    width: 116px;
-    height: 80px;
-    position: absolute;
-}
-#logoPicture {
-    width: 116px;
-    height: 80px;
-}
+
 </style>
     <head>
         <title>
