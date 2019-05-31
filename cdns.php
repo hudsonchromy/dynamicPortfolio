16991e8c254a37b8ebeb3ded7ae49b4ac453faa1
hudsonchromy <hjc0018@auburn.edu>
2019-05-31 09:18:47 -0500

    added jquery mobile

--- cdns.php
+++ cdns.php
@@ -2,6 +2,7 @@
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-139847415-2"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/trianglify/1.1.0/trianglify.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/trianglify/0.4.0/trianglify.min.js"></script>
+<meta content="width=device-width, initial-scale=1" name="viewport" />
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
