ba63a39a903bcc2ce99adfa1cb13625759f0e7b6
hudsonchromy <hjc0018@auburn.edu>
2019-06-02 07:27:41 -0500

    update

--- skills/index.php
+++ skills/index.php
@@ -1,17 +1,9 @@
 <html>
     <head>
         <title>
-        Hudson Chromy
+            Skills - Hudson Chromy
         </title>
         <link rel="stylesheet" type="text/css" href="../style.css">
-        <link rel="stylesheet" href="https://cdn.auburn.edu/assets/css/bootstrap.min.css"/>
-        <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Kadwa:700&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Inconsolata:700&display=swap" rel="stylesheet">
-        <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/trianglify/1.1.0/trianglify.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/trianglify/0.4.0/trianglify.min.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <?php include('../cdns.php');?>
     </head>
@@ -19,34 +11,103 @@
     <?php include('../menu.php');?>
     <div class="col-sm-12">
         <?php include('../sidebar.php')?>
-            <div class="col-sm-9">
-            <div class="col-sm-12">
+            <div class="col-sm-12 skillCard">
                 <?php
+                    $i = 0;
                     $files = scandir('../images/skills/');
                     foreach($files as $file):
                     if(pathinfo($file, PATHINFO_EXTENSION) != "png") {
                         continue;
                     }
+                    $i++;
+                    if ($i%3 == 1) {
+                        echo "<div class='col-sm-3 skill'>";
+                    }
                 ?>
-                <div class="col-sm-3 skill">
-                    <img src="../images/skills/<? echo $file;?>">
-                </div>
-                <? endforeach;?>
+                    <img id="<?php echo 'skill'.$i?>" src="../images/skills/<? echo $file;?>">
+                <? 
+                if ($i%3 == 0) {
+                    echo "</div>";
+                }
+                endforeach;?>
             </div>
         </div>
     </div>
 </body>
 <script>
-			var pattern = Trianglify({
-				height: window.innerHeight,
-				width: window.innerWidth,
-				cell_size: 80 + Math.random() * 70,
-				variance: 0.2 + Math.random(),
-				stroke_width: 1.8 + Math.random(),
-				x_colors: 'Greys',
-				y_colors: 'match_x'
-			});
-			document.body.style.background = 'url(' + pattern.canvas().toDataURL() + ')';
-			document.body.style.backgroundAttachment = 'fixed';
-		</script>
+window.onload = function() {
+    myMove();
+}
+function myMove() {
+    class Skill {
+        constructor(num, below) {
+            this.below = below;
+            this.top = 0;
+            this.num = num;
+        }
+        atBottom() {
+            let diff = 0;
+            for (var j = 0; j < this.below.length; j++) {
+                console.log(this.below[j]);
+                diff += skills[this.below[j]].height() + 40;
+            }
+            return($('html').height() - diff > document.getElementById("skill" + this.num).getBoundingClientRect().bottom);
+        }
+        height() {
+            return(document.getElementById('skill' + this.num).clientHeight);
+        }
+    }
+    var id = setInterval(frame, 2);
+    var skills = [new Skill(0, []), new Skill(1, [2,3]), new Skill(2, [3]), new Skill(3, []), new Skill(4, [5,6]), new Skill(5, [6]), new Skill(6, []), new Skill(7, [8,9]), new Skill(8, [9]), new Skill(9, []), new Skill(10, [11]), new Skill(11, []), new Skill(12, [])];
+    var row = 3;
+    var speed = 1.5;
+    function frame() {
+        speed += 0.015;
+        var changed = false;
+        for (i = 0; i < 4; i++) {
+            if (row + i * 3 == 12) {
+                continue;
+            }
+            if (skills[row + i * 3].atBottom()) {
+                skills[row + i * 3].top += speed;
+                setTop(row + i * 3);
+                if (row != 3) {
+                    skills[(row + 1) + i * 3].top -= speed;
+                    setTop((row + 1) + i * 3);
+                    
+                }
+                changed = true;
+            }
+        }
+        if (!changed) {
+            speed = 1
+            row -= 1;
+            console.log('here');
+            if (row == 0) {
+                clearInterval(id);
+            }
+        }
+    }
+
+    function setTop(skill) {
+        if (skill < 12 && skill > 0) {
+            $('#skill' + skill).css('margin-top', skills[skill].top);
+        }
+    }
+    
+}
+</script>
+<script>
+    var pattern = Trianglify({
+        height: window.innerHeight,
+        width: window.innerWidth,
+        cell_size: 80 + Math.random() * 70,
+        variance: 0.2 + Math.random(),
+        stroke_width: 1.8 + Math.random(),
+        x_colors: 'Greys',
+        y_colors: 'match_x'
+    });
+    document.body.style.background = 'url(' + pattern.canvas().toDataURL() + ')';
+    document.body.style.backgroundAttachment = 'fixed';
+</script>
 </html>
