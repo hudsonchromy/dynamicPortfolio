91ba95dfbee42e2ab63d1fcad5fc65a7a7ef4f4d
hudsonchromy <hjc0018@auburn.edu>
2019-06-02 07:31:01 -0500

    added more from files

--- cdns.php
+++ cdns.php
@@ -2,6 +2,14 @@
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-139847415-2"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/trianglify/1.1.0/trianglify.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/trianglify/0.4.0/trianglify.min.js"></script>
+<link rel="stylesheet" href="https://cdn.auburn.edu/assets/css/bootstrap.min.css"/>
+<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Kadwa:700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Inconsolata:700&display=swap" rel="stylesheet">
+<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/trianglify/1.1.0/trianglify.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/trianglify/0.4.0/trianglify.min.js"></script>
 <meta content="width=device-width, initial-scale=1" name="viewport" />
 <script>
   window.dataLayer = window.dataLayer || [];
@@ -10,3 +18,4 @@
 
   gtag('config', 'UA-139847415-2');
 </script>
+
