f622660ef92e8618ec5ecb7c7785758c06bfd722
hudsonchromy <hjc0018@auburn.edu>
2019-06-02 07:29:20 -0500

    update

--- contact/index.php
+++ contact/index.php
@@ -1,27 +1,22 @@
 <html>
     <head>
         <title>
-        Contact Me
+            Contact Me - Hudson Chromy
         </title>
         <link rel="stylesheet" type="text/css" href="../style.css">
-        <link rel="stylesheet" href="https://cdn.auburn.edu/assets/css/bootstrap.min.css"/>
         <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Kadwa:700&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Inconsolata:700&display=swap" rel="stylesheet">
-        <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
         <link href="https://fonts.googleapis.com/css?family=Berkshire+Swash&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Berkshire+Swash|BioRhyme+Expanded:800|Bungee+Outline|Bungee+Shade|Creepster|Miltonian+Tattoo|Podkova:700|Rye|Slackey&display=swap" rel="stylesheet">
         <?php include('../cdns.php');?>
         <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Fredericka+the+Great&display=swap" rel="stylesheet">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/trianglify/1.1.0/trianglify.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/trianglify/0.4.0/trianglify.min.js"></script>
     </head>
 <body>
-    <?php include('../menu.php');?>
+<?php include('../menu.php');?>
     <div class="col-sm-12">
         <?php include('../sidebar.php')?>
-            <div class="col-sm-9">
             <div class="section">
                 <h1>Contact Me</h1>
                 <a href="https://www.linkedin.com/in/hchromy">
@@ -33,7 +28,7 @@
                 </a>
                 <p>Github</p>
                 <img src="../images/email.png" alt="Email Icon">
-                <p>hjc0018@auburn.edu</p>
+                <p>hudsonchromy@gmail.com</p>
             </div>
         <div>
     </div>
