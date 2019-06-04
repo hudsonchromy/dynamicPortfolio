cd8498ecd3c0392c82ac3042063d82e3d064ba44
hudsonchromy <hjc0018@auburn.edu>
2019-06-04 13:39:16 -0500

    adding page

--- portfolio/polyfold/index.php
+++ portfolio/polyfold/index.php
@@ -10,13 +10,13 @@
         <link href="https://fonts.googleapis.com/css?family=Inconsolata:700&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Berkshire+Swash&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Berkshire+Swash|BioRhyme+Expanded:800|Bungee+Outline|Bungee+Shade|Creepster|Miltonian+Tattoo|Podkova:700|Rye|Slackey&display=swap" rel="stylesheet">
-        <?php include('../../cdns.php');?>
+        <?php include('/cdns.php');?>
         <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Fredericka+the+Great&display=swap" rel="stylesheet">
     </head>
 <body>
-<?php include('../../menu.php');?>
+<?php include('/menu.php');?>
     <div class="col-sm-12">
-        <?php include('../../sidebar.php')?>
+        <?php include('/sidebar.php')?>
             <div class="section">
                 <h1>Polyfold</h1>
                 <img src="/images/portfolio/polyfold/polyfold.gif" alt="Polyfold" style="width: 80%;">
@@ -24,22 +24,6 @@
         <div>
     </div>
 </body>
-<script>
-    window.onload = function() {
-        $('#firstLetter').css('font-family', '"Berkshire Swash", cursive');
-        changeLetter();
-    }
-    function changeLetter() {
-        var fonts = ["'Berkshire Swash', cursive", "Rye', cursive", "Podkova', serif", "'Bungee Shade', cursive", "'Slackey', cursive", "'Miltonian Tattoo', cursive", "'Bungee Outline', cursive", "'BioRhyme Expanded', serif", "'Fredericka the Great', cursive", "'Abril Fatface', cursive"];
-        var id = setInterval(frame, 1000);
-        var currFont = 0;
-        var fontOn = 0;
-        function frame() {
-            fontOn++;
-            $('#firstLetter').css('font-family', fonts[fontOn % (fonts.length - 1)]);
-        }
-    }
-</script>
     <script>
 			var pattern = Trianglify({
 				height: window.innerHeight,
