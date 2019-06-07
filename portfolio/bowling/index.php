5c1d2a65ccb3a689be692c64d779639ae668fa51
hudsonchromy <hjc0018@auburn.edu>
2019-06-07 10:04:25 -0500

    changing font removed

--- portfolio/bowling/index.php
+++ portfolio/bowling/index.php
@@ -1,13 +1,7 @@
-<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
-
-
-
-
-
 <html>
     <head>
         <title>
-            Contact Me - Hudson Chromy
+            Bowling - Hudson Chromy
         </title>
         <link rel="stylesheet" type="text/css" href="../../style.css">
         <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
@@ -17,6 +11,7 @@
         <link href="https://fonts.googleapis.com/css?family=Berkshire+Swash&display=swap" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Berkshire+Swash|BioRhyme+Expanded:800|Bungee+Outline|Bungee+Shade|Creepster|Miltonian+Tattoo|Podkova:700|Rye|Slackey&display=swap" rel="stylesheet">
         <?php include('../../cdns.php');?>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
         <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Fredericka+the+Great&display=swap" rel="stylesheet">
     </head>
 <body>
@@ -28,22 +23,6 @@
     </div>
 </div>
 </body>
-<script>
-    window.onload = function() {
-        $('#firstLetter').css('font-family', '"Berkshire Swash", cursive');
-        changeLetter();
-    }
-    function changeLetter() {
-        var fonts = ["'Berkshire Swash', cursive", "Rye', cursive", "Podkova', serif", "'Bungee Shade', cursive", "'Slackey', cursive", "'Miltonian Tattoo', cursive", "'Bungee Outline', cursive", "'BioRhyme Expanded', serif", "'Fredericka the Great', cursive", "'Abril Fatface', cursive"];
-        var id = setInterval(frame, 1000);
-        var currFont = 0;
-        var fontOn = 0;
-        function frame() {
-            fontOn++;
-            $('#firstLetter').css('font-family', fonts[fontOn % (fonts.length - 1)]);
-        }
-    }
-</script>
     <script>
 			var pattern = Trianglify({
 				height: window.innerHeight,
@@ -51,7 +30,7 @@
 				cell_size: 80 + Math.random() * 70,
 				variance: 0.2 + Math.random(),
 				stroke_width: 1.8 + Math.random(),
-				x_colors: 'Greys',
+				x_colors: ["#545454", "#2a2a2a", "#000000"],
 				y_colors: 'match_x'
 			});
 			document.body.style.background = 'url(' + pattern.canvas().toDataURL() + ')';
@@ -74,7 +53,7 @@
     var second = [];
     var third = [];
     for(let i = 0; i < scores.length; i++) {
-        nums.push(i);
+        nums.push(i + 1);
         if (i % 3 == 0) {
             console.log('here');
             first.push(scores[i]);
